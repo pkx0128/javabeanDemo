4ba541e568bee8a895ac3fa03205b738d788a06f
pankx <764670547@qq.com>
2019-09-01 18:51:41 +0800

    初始化项目并创建JavaBeanDemo

---
+++ WebContent/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Welcome page</title>
+</head>
+<body>
+<h1>javaBean Project</h1>
+</body>
+</html>
---
+++ src/JavaBean/JavaBeanDemo1.java
@@ -0,0 +1,32 @@
+package JavaBean;
+
+public class JavaBeanDemo1 {
+	private String name;
+	private int age;
+	private String sex;
+	
+	public JavaBeanDemo1() {}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getAge() {
+		return this.age;
+	}
+	
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getSex() {
+		return this.sex;
+	}
+	
+	
+}
